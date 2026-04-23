e5734340726dd5364d192ef75dece2edf3176477
99JIWONDEV <99jiwonpark@gmail.com>
2026-04-22 21:31:18 -0400

    feat: add empty line & delete space for CI

--- src/HaneulCoin.sol
+++ src/HaneulCoin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 // 0.8.20 이상의 solidity로 컴파일한다
-pragma solidity ^0.8.20; 
+pragma solidity ^0.8.20;
 
 // ERC20을 이 파일에서 쓰기 위해서 리맵핑 작업 (ERC20만 해당 파일에서 가져옴)
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
